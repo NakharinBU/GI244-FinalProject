53501f08b2c1e5b6a5dfd4fd30473161aeb6fcb3
NakharinBU <nakharin.sise@bumail.net>
2025-05-09 17:32:55 +0700

    commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,88 +6,83 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
 
+    // [6] set the range of the player's movement in x-axis
     public float xRange = 10;
 
+    // [8] declare Projectile prefab variable
     public GameObject projectilePrefab;
 
     private float horizontalInput;
 
+    // [1] declare a private InputAction variable
     private InputAction moveAction;
+    // [10] declare a private InputAction variable for shooting
     private InputAction shootAction;
 
     private bool hasPowerUp = false;
-    private bool hasSlowTime = false;
-
-    private List<MoveForward> moveTargets = new List<MoveForward>();
 
     private void Awake()
     {
+        // [2] find the action by name
+        // this is to optimize the search for the action
         moveAction = InputSystem.actions.FindAction("Move");
+
+        // [11] find the action by name
         shootAction = InputSystem.actions.FindAction("Shoot");
     }
 
+    // Update is called once per frame
     void Update()
     {
+        // [3] use input system to get horizontal input
         horizontalInput = moveAction.ReadValue<Vector2>().x;
+
+        // [4] move the player
         transform.Translate(horizontalInput * speed * Time.deltaTime * Vector3.right);
 
-        if (transform.position.x < -xRange)
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
-        if (transform.position.x > xRange)
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
 
+        // [5] keep the player inbounds
+        // if (transform.position.x < -10)
+        // {
+        //     transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        // }
+
+        // [7] keep the player inbounds using xRange variable
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        // [12] check if the player is shooting
         if (shootAction.triggered)
         {
+            // [13] spawn a projectile
+            //Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
             GameObject p = ProjectileObjectPool.GetInstance().Acquire();
             p.transform.SetLocalPositionAndRotation(transform.position, p.transform.rotation);
         }
     }
-
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("PowerUp"))
+        if (other.gameObject.CompareTag("PowerUp"))
         {
-            Debug.Log("Speed");
+            Debug.Log("GG");
             Destroy(other.gameObject);
             hasPowerUp = true;
             StartCoroutine(PowerUpCooldown(5f));
             speed = 30;
-        }
-        else if (other.CompareTag("SlowTime"))
-        {
-            Debug.Log("SlowTime");
-            Destroy(other.gameObject);
-            hasSlowTime = true;
-
-            moveTargets.Clear();
-            moveTargets.AddRange(FindObjectsByType<MoveForward>(FindObjectsSortMode.None));
-
-            foreach (var target in moveTargets)
-            {
-                target.speed = 1f;
-            }
-
-            StartCoroutine(SlowTime(5f));
+            
         }
     }
-
     IEnumerator PowerUpCooldown(float cooldownTime)
     {
         yield return new WaitForSeconds(cooldownTime);
         hasPowerUp = false;
         speed = 10;
     }
-
-    IEnumerator SlowTime(float cooldownTime)
-    {
-        yield return new WaitForSeconds(cooldownTime);
-        hasSlowTime = false;
-
-        foreach (var target in moveTargets)
-        {
-            if (target != null) target.speed = 4f; 
-        }
-
-        Debug.Log("Speed Restored");
-    }
+    
 }
